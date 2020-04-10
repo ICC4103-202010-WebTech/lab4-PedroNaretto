94707f9782b5fbbbd6f7bb05cdd50f9e7f65214a
PedroNaretto <ppnaretto@miuandes.cl>
2020-04-09 21:15:58 -0400

    error callback

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -5,6 +5,7 @@ class Ticket < ApplicationRecord
   # TODO: complete the folowing
   before_create :update_stats_increase
   before_destroy :update_stats_decrease
+  before_validation :maximum
 
   private
     def update_stats_increase
@@ -18,5 +19,14 @@ class Ticket < ApplicationRecord
       es.tickets_sold -=1
       es.attendance -=1
       es.save!
+    end
+    def maximum
+      ev = self.ticket_type.event.event_venue
+      es = self.ticket_type.event.event_stat
+      if es.tickets_sold == ev.capacity
+        puts("Error: Tickets are sold out")
+        errors.add(:base, "Tickets are sold out")
+        throw(:abort)
+      end
     end
 end
